56a7aac08a443ccea147b38dd3a74dff22c813b3
sunven <604164227@qq.com>
2024-09-18 23:13:59 +0800

    feat: env

--- lib/db.ts
+++ lib/db.ts
@@ -1,136 +1,136 @@
-// import prisma from './prisma'
+import prisma from './prisma'
 
-// export interface SoftwareInput {
-//   name: string
-//   description?: string
-//   website?: string
-//   icon?: string
-//   categoryId: number
-//   tags: string[]
-// }
+export interface SoftwareInput {
+  name: string
+  description?: string
+  website?: string
+  icon?: string
+  categoryId: number
+  tags: string[]
+}
 
-// // 创建新软件
-// export async function createSoftware(data: SoftwareInput) {
-//   const { tags, ...softwareData } = data
-//   const software = await prisma.software.create({
-//     data: {
-//       ...softwareData,
-//       tags: {
-//         create: tags.map(tag => ({
-//           tag: {
-//             connectOrCreate: {
-//               where: { name: tag },
-//               create: { name: tag },
-//             },
-//           },
-//         })),
-//       },
-//     },
-//     include: {
-//       category: true,
-//       tags: {
-//         include: {
-//           tag: true,
-//         },
-//       },
-//     },
-//   })
+// 创建新软件
+export async function createSoftware(data: SoftwareInput) {
+  const { tags, ...softwareData } = data
+  const software = await prisma.software.create({
+    data: {
+      ...softwareData,
+      tags: {
+        create: tags.map(tag => ({
+          tag: {
+            connectOrCreate: {
+              where: { name: tag },
+              create: { name: tag },
+            },
+          },
+        })),
+      },
+    },
+    include: {
+      category: true,
+      tags: {
+        include: {
+          tag: true,
+        },
+      },
+    },
+  })
 
-//   return {
-//     ...software,
-//     tags: software.tags.map(t => t.tag.name),
-//   }
-// }
+  return {
+    ...software,
+    tags: software.tags.map(t => t.tag.name),
+  }
+}
 
-// // 获取所有软件
-// export async function getAllSoftware() {
-//   const softwares = await prisma.software.findMany({
-//     include: {
-//       category: true,
-//       tags: {
-//         include: {
-//           tag: true,
-//         },
-//       },
-//     },
-//   })
+// 获取所有软件
+export async function getAllSoftware() {
+  const softwares = await prisma.software.findMany({
+    include: {
+      category: true,
+      tags: {
+        include: {
+          tag: true,
+        },
+      },
+    },
+  })
 
-//   return softwares.map(software => ({
-//     ...software,
-//     tags: software.tags.map(t => t.tag.name),
-//   }))
-// }
+  return softwares.map(software => ({
+    ...software,
+    tags: software.tags.map(t => t.tag.name),
+  }))
+}
 
-// // 获取单个软件
-// export async function getSoftware(id: number) {
-//   const software = await prisma.software.findUnique({
-//     where: { id },
-//     include: {
-//       category: true,
-//       tags: {
-//         include: {
-//           tag: true,
-//         },
-//       },
-//     },
-//   })
+// 获取单个软件
+export async function getSoftware(id: number) {
+  const software = await prisma.software.findUnique({
+    where: { id },
+    include: {
+      category: true,
+      tags: {
+        include: {
+          tag: true,
+        },
+      },
+    },
+  })
 
-//   if (!software) return null
+  if (!software) return null
 
-//   return {
-//     ...software,
-//     tags: software.tags.map(t => t.tag.name),
-//   }
-// }
+  return {
+    ...software,
+    tags: software.tags.map(t => t.tag.name),
+  }
+}
 
-// // 更新软件
-// export async function updateSoftware(id: number, data: SoftwareInput) {
-//   const { tags, ...softwareData } = data
-//   const software = await prisma.software.update({
-//     where: { id },
-//     data: {
-//       ...softwareData,
-//       tags: {
-//         deleteMany: {},
-//         create: tags.map(tag => ({
-//           tag: {
-//             connectOrCreate: {
-//               where: { name: tag },
-//               create: { name: tag },
-//             },
-//           },
-//         })),
-//       },
-//     },
-//     include: {
-//       category: true,
-//       tags: {
-//         include: {
-//           tag: true,
-//         },
-//       },
-//     },
-//   })
+// 更新软件
+export async function updateSoftware(id: number, data: SoftwareInput) {
+  const { tags, ...softwareData } = data
+  const software = await prisma.software.update({
+    where: { id },
+    data: {
+      ...softwareData,
+      tags: {
+        deleteMany: {},
+        create: tags.map(tag => ({
+          tag: {
+            connectOrCreate: {
+              where: { name: tag },
+              create: { name: tag },
+            },
+          },
+        })),
+      },
+    },
+    include: {
+      category: true,
+      tags: {
+        include: {
+          tag: true,
+        },
+      },
+    },
+  })
 
-//   return {
-//     ...software,
-//     tags: software.tags.map(t => t.tag.name),
-//   }
-// }
+  return {
+    ...software,
+    tags: software.tags.map(t => t.tag.name),
+  }
+}
 
-// // 删除软件
-// export async function deleteSoftware(id: number) {
-//   await prisma.software.delete({
-//     where: { id },
-//   })
-// }
+// 删除软件
+export async function deleteSoftware(id: number) {
+  await prisma.software.delete({
+    where: { id },
+  })
+}
 
-// // 获取所有分类
-// export async function getAllCategories() {
-//   return prisma.category.findMany()
-// }
+// 获取所有分类
+export async function getAllCategories() {
+  return prisma.category.findMany()
+}
 
-// // 获取所有标签
-// export async function getAllTags() {
-//   return prisma.tag.findMany()
-// }
+// 获取所有标签
+export async function getAllTags() {
+  return prisma.tag.findMany()
+}
