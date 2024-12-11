b06591eae05a782f17e4d7901f969dbca7620e35
wenquansun <604164227@qq.com>
2024-12-11 10:41:11 +0800

    feat: add cloudflare/next-on-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -8,5 +10,9 @@ const nextConfig = {
   },
   reactStrictMode: false,
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
